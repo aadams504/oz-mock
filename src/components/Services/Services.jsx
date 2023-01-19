cb315d7fc2e27b6757029785f3d9ebddaf6fdff6
Arianna <95667502+aadams504@users.noreply.github.com>
2023-01-19 11:19:40 -0800

    add services component

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,16 @@
+import { ServicesContainer } from "../../components/Services/Services.styles";
+import { SectionTitleContaier } from "../component.styles";
+
+const Services = () => {
+  return (
+    <>
+      <h2>Humanitarian</h2>
+      <ServicesContainer>
+        <h1>Hello</h1>
+        <h4>World</h4>
+      </ServicesContainer>
+    </>
+  );
+};
+
+export default Services;
---
+++ src/components/Services/Services.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ServicesContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  margin-left: auto;
+  margin-right: auto;
+`;
